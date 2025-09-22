387e720f566e88612ff1aa60bf2f0e07530aee04
Real_Marshal <remarshal@protonmail.com>
2025-09-22 14:19:15 +0300

    temp fix pressable not always firing onPress with animated view children

--- components/FloatingMenu.tsx
+++ components/FloatingMenu.tsx
@@ -1,8 +1,14 @@
-import { Animated, Pressable, Text, View } from 'react-native'
+import { Pressable, Text, View } from 'react-native'
 import Feather from '@expo/vector-icons/Feather'
 import { colors } from '@/common/theme'
 import { cn } from '@/common/utils/css'
-import { ReactNode, useEffect, useRef } from 'react'
+import { ReactNode, useEffect } from 'react'
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated'
 
 export function FloatingMenuItem({
   description,
@@ -64,42 +70,25 @@ export function FloatingButton({
   activeColor: string
   active?: boolean
 }) {
-  // cant fucking merge the fix in 2 fucking months, dumbasses
-  // https://github.com/facebook/react-native/issues/51621
-  //
-  // const rot = useSharedValue(0)
-  //
-  // useEffect(() => {
-  //   rot.value = withTiming(active ? 1 : 0, { duration: 150 })
-  // }, [active, rot])
-  //
-  // const animatedStyle = useAnimatedStyle(() => ({
-  //   transform: [{ rotate: `${interpolate(rot.value, [0, 1], [0, 45])}deg` }],
-  // }))
-
-  const rotationAnim = useRef(new Animated.Value(0)).current
+  const rot = useSharedValue(0)
 
   useEffect(() => {
-    Animated.timing(rotationAnim, {
-      toValue: active ? 1 : 0,
-      duration: 150,
-      useNativeDriver: true,
-    }).start()
-  }, [active, rotationAnim])
+    rot.value = withTiming(active ? 1 : 0, { duration: 150 })
+  }, [active, rot])
 
-  const rotationInterpolate = rotationAnim.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0deg', '45deg'],
-  })
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${interpolate(rot.value, [0, 1], [0, 45])}deg` }],
+  }))
 
   return (
-    <Pressable onPress={onPress}>
+    // temp fix for https://github.com/facebook/react-native/issues/51621
+    <Pressable onPressIn={onPress}>
       {({ pressed }) => (
         <View
           className='p-[14px] rounded-full absolute right-safe-offset-6 bottom-safe-offset-6'
           style={{ backgroundColor: pressed || active ? activeColor : color }}
         >
-          <Animated.View style={{ transform: [{ rotate: rotationInterpolate }] }}>
+          <Animated.View style={animatedStyle}>
             <Feather name='plus' size={26} color={colors.bg} />
           </Animated.View>
         </View>
